c6f11c7aef3a04e1a3751fcab1f3c429b5050e2d
Aether <aether@pierdoling.org>
2026-09-07 08:46:00 +0300

    0.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,8 +130,8 @@ android {
         applicationId = "com.material.xray"
         minSdk = 28
         targetSdk = 36
-        versionCode = 801
-        versionName = "0.8.1"
+        versionCode = 802
+        versionName = "0.8.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
